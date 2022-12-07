a9edd3711fad076f9558fadf519f7e910af7ee1b
MariaEduardaSeichas <oliveiramariaeduarda3478@gmail.com>
2022-12-07 16:36:08 -0300

    alteração

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
-let cacheName = "trabalho final";
-let filesToCache = ["/", "/index.html",
-                "/css/style.css", "/js/main.js"];
+//let cacheName = "trabalho final";
+//let filesToCache = ["/", "/index.html",
+                //"/css/style.css", "/js/main.js"];
 
 /* inicializando a service worker e fazendo o 
 download do conteúdo da aplicação */
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+//self.addEventListener("install", (e) => {
+  //e.waitUntil(
+    //caches.open(cacheName).then(function (cache) {
+      //return cache.addAll(filesToCache);
+    //})
+  //);
+//});
 
 /* disponibilizando o conteudo quando estiver offline */
-self.addEventListener("fetch", (e) => {
-  const req = e.request;
-  const url = new URL(req.url);
-  if(url.origin === location.origin) {
-      e.respondWith(cacheFirst(req));
-    }else {
-      e.respondWith(networkFirst(req));
-    }
-});
+//self.addEventListener("fetch", (e) => {
+  //const req = e.request;
+  //const url = new URL(req.url);
+  //if(url.origin === location.origin) {
+      //e.respondWith(cacheFirst(req));
+    //}else {
+      //e.respondWith(networkFirst(req));
+    //}
+//});
 
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "index";
